f926079d1ba07770ae3735110415c19af143cd85
Pierre-Rudolf Gerlach <pierre@joshfire.com>
2013-02-11 16:06:10 +0100

    add node install to setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,14 +2,33 @@
 
 cd $(dirname $0)
 
-mkdir python
+## setup
+
+NODE_VERSION=0.8.12
+
+## python
+## assumes we do have virtualenv
+
+mkdir -p python
 virtualenv python
 
-cat << EOF > ./activate
-. $PWD/python/bin/activate
-EOF
-chmod +x ./activate
-
-. ./activate
+. ./python/bin/activate
 
 pip install -U --use-mirrors -r ./requirements.txt
+
+## node
+## assumes we do have nvm installed
+
+. ~/.nvm/nvm.sh
+nvm install ${NODE_VERSION}  # may fail if already installed, but no error code returned
+nvm use ${NODE_VERSION} # 
+
+## global activate file
+
+rm -f ./activate
+cat << EOF >> ./activate
+. $PWD/python/bin/activate
+. ~/.nvm/nvm.sh
+nvm use ${NODE_VERSION}
+EOF
+chmod +x ./activate
